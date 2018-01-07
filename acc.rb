d1850ccb6b4eb6679ed1c4a47a394ca1333b3fec
ameba23 <ameba23@systemli.org>
2018-01-07 14:31:29 +0100

    got it working with floats, need to change back to bigdecimal

--- acc.rb
+++ acc.rb
@@ -12,22 +12,21 @@ def coins_by_bittrex
 end
 
 def fiat_btc_rate(iso_currency = nil)
-   BigDecimal('1.0') / BigDecimal.new(
-    coinbase_rates["data"]["rates"][iso_currency || code]
-   )
+   1.0 / coinbase_rates["data"]["rates"][iso_currency || code].to_f
 end
 
 def coinbase_rates
   HTTParty.get("https://api.coinbase.com/v2/exchange-rates?currency=BTC").parsed_response
 end
 
-total = BigDecimal('0')
+total = 0.0
 data = CSV.read('figs.csv')
+
 data.each do |row|
   puts "#{row}"
   if ['GBP','EUR','USD'].include?(row[0])
     puts "is fiat"
-    btc_amount = BigDecimal(row[1]) * fiat_btc_rate(row[0])
+    btc_amount = row[1].to_f * fiat_btc_rate(row[0])
     puts "#{btc_amount}"
     total += btc_amount
   else
@@ -35,16 +34,17 @@ data.each do |row|
       puts "adding #{row[1].to_f}"
       total += row[1].to_f
     else
-      btc_amount = BigDecimal(row[1]) * fiat_btc_rate(row[0])
+      btc_amount = row[1].to_f * fiat_btc_rate(row[0])
       puts "non fiat: #{btc_amount}"
       total += btc_amount
     end
   end
 end
-puts "total in btc: #{total}"
+puts "total gain in btc: #{total}"
 eurrate = fiat_btc_rate("EUR")
-puts "euro rate #{eurrate}"
-totaleur = total * eurrate
+puts "btc to euro rate: %.2f" % eurrate
+totaleur = total / eurrate
 #puts "total in eur: #{totaleur.truncate(2).to_s('F')}"
-puts "total in eur: %.2f" % totaleur
+puts "total gain in eur: %.2f" % totaleur
+
 binding.pry
