fa1484645c189e4ffb284832bed8e4358a4578da
ameba23 <ameba23@systemli.org>
2018-01-07 15:40:25 +0100

    sorted a few things, added comment field

--- acc.rb
+++ acc.rb
@@ -2,6 +2,10 @@ require 'httparty'
 require 'pry'
 require 'csv'
 
+
+# manage assets from csv
+# todo -destinguish between spent and holding?
+
 def bittrex_rates
   HTTParty.get("https://bittrex.com/api/v1.1/public/getmarketsummaries").parsed_response
 end
@@ -20,26 +24,40 @@ def coinbase_rates
   HTTParty.get("https://api.coinbase.com/v2/exchange-rates?currency=BTC").parsed_response
 end
 
+def displayrow(curr,btc_amount)
+  puts " #{curr[0]} #{curr[1]} = BTC %.5f #{curr[2]}" % btc_amount
+end
 total = 0.0
 data = CSV.read('figs.csv')
 
+rates_got = {}
+
 data.each do |row|
   if ['GBP','EUR','USD'].include?(row[0])
-    btc_amount = row[1].to_f * fiat_btc_rate(row[0])
-    puts " #{row[0]} #{row[1]} = BTC #{btc_amount}"
+    if !rates_got.key?(row[0])
+       rates_got[row[0]] = fiat_btc_rate(row[0])
+    end
+    the_rate = rates_got[row[0]]
+    btc_amount = row[1].to_f * the_rate
+    displayrow(row, btc_amount)
     total += btc_amount
   else
     if row[0] == 'BTC' 
-      puts " #{row[0]} #{row[1]} = BTC #{row[1].to_f}"
+      displayrow(row, row[1].to_f)
       total += row[1].to_f
     else
-      btc_amount = row[1].to_f * fiat_btc_rate(row[0])
-      puts " #{row[0]} #{row[1]} = BTC #{btc_amount}"
+      if !rates_got.key?(row[0])
+         rates_got[row[0]] = fiat_btc_rate(row[0])
+      end
+      the_rate = rates_got[row[0]]
+      btc_amount = row[1].to_f * the_rate
+      displayrow(row, btc_amount)
       total += btc_amount
     end
   end
 end
-puts "total gain in btc: #{total}"
+puts "-----------------------------------"
+puts "total gain in btc: %.5f " % total
 eurrate = fiat_btc_rate("EUR")
 puts "btc to euro rate: %.2f" % eurrate
 totaleur = total / eurrate
